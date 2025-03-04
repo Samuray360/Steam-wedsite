124546cc630e79f09ddec0de48ac6f8dc5f00caf
Samuray360 <151565282+Samuray360@users.noreply.github.com>
2025-03-04 14:29:19 -0400

    update

--- 3-DHelpers.py
+++ 3-DHelpers.py
@@ -14,26 +14,39 @@ def main(page: ft.Page):
         fit=ft.ImageFit.CONTAIN  
     )
 
-    def home_function():
-        pass
-    def about_us_function():
-        pass
+    about_view = ft.Container(
+        bgcolor="black",
+        content=ft.Text("About Us: Here is some information."),
+        visible=False,
+        padding=20,
+        width=800,
+        height=800
+    )
+    def home_function(about_view):
+        about_view.visible=False
+        page.update
+    def about_us_function(about_view):
+        about_view.visible=True
+        page.update
     def donate_function():
-        pass
+
+        page.update
+
 
     home_button=ft.ElevatedButton(text="Home",on_click=home_function)
-    donate_button=ft.ElevatedButton(text="Home",on_click=donate_function)
-    about_button=ft.ElevatedButton(text="Home",on_click=about_us_function)
+    donate_button=ft.ElevatedButton(text="Donate",on_click=donate_function)
+    about_button=ft.ElevatedButton(text="About",on_click=about_us_function)
 
     button_row = ft.Row(
         controls=[
             home_button,
-            donate_button,
-            about_button
+            about_button,
+            donate_button
+           
         ],
         alignment=ft.MainAxisAlignment.END  
     )
-
+    
   
     button_container = ft.Container(
         content=button_row,  
@@ -43,7 +56,7 @@ def main(page: ft.Page):
         alignment=ft.alignment.top_right
     )
 
-    page.add(button_container,img)
+    page.add(button_container,img,about_view)
     page.update()
 
 ft.app(target=main)
