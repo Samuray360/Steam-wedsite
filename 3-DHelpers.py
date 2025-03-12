90209e9272aaf0aa78f0a5ab1377772e52a411db
Samuray360 <151565282+Samuray360@users.noreply.github.com>
2025-03-12 13:33:26 -0400

    home interface

--- 3-DHelpers.py
+++ 3-DHelpers.py
@@ -9,19 +9,30 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.START
 
    
-
+    logo=ft.Image(
+            src="logo.png" ,
+            width=80,
+            height=80,
+            # alignment=ft.MainAxisAlignment.START
+        )
+    
+    img = ft.Image(
+        src="Home.jpg",
+        width=page.width,
+        height=page.height,
+    )
     about_view = ft.Container(
        
         bgcolor="black",
         content=ft.Text("About Us: Here is some information."),
         visible=False,
-        padding=20,
-        width=800,
-        height=800
+        # padding=20,
+        width=page.width,
+        height=page.height
     )
 
     home_view=ft.Container(
-    #    img
+       img
             
     )
     pay_way=ft.Container(
@@ -34,25 +45,26 @@ def main(page: ft.Page):
 
     def home_function(e):
         about_view.visible=False
-        home_view=True
+        home_view.visible=True
         page.update()
     def about_us_function(e):
-        home_view=False
+        home_view.visible=False
         about_view.visible=True
         page.update()
     def donate_function(e):
 
         page.update()
 
-    card_number=ft.TextField,
-    owner_name=ft.TextField,
-    due_date=ft.TextField,
-    home_button=ft.ElevatedButton(text="Home",on_click=home_function,bgcolor="4093DF")
-    donate_button=ft.ElevatedButton(text="Donate",on_click=donate_function,bgcolor="4093DF")
-    about_button=ft.ElevatedButton(text="About",on_click=about_us_function,bgcolor="4093DF")
+    card_number=ft.TextField()
+    owner_name=ft.TextField()
+    due_date=ft.TextField()
+    home_button=ft.ElevatedButton(text="Home",on_click=home_function,style=ft.ButtonStyle( bgcolor="#175ABF" ,color=ft.colors.WHITE))
+    donate_button=ft.ElevatedButton(text="Donate",on_click=donate_function,style=ft.ButtonStyle (bgcolor="#175ABF",color=ft.colors.WHITE))
+    about_button=ft.ElevatedButton(text="About",on_click=about_us_function,style=ft.ButtonStyle (bgcolor="#175ABF",color=ft.colors.WHITE))
 
     button_row = ft.Row(
         controls=[
+            logo,
             home_button,
             about_button,
             donate_button
@@ -63,14 +75,10 @@ def main(page: ft.Page):
     
   
     button_container = ft.Container(
-        # logo=ft.Image(
-        #     src="C:\Users\ethan\OneDrive\Desktop\Java\Steam-wedsite\Screenshot 2025-03-10 141247.png"  ,
-        #     width=20,
-        #     height=20,
-        # ),
+     
         content=button_row,  
-        bgcolor="#4093DF",
-        padding=20, 
+        bgcolor="#175ABF",
+        # padding=20, 
         width=page.width,
         alignment=ft.alignment.top_center
     )
